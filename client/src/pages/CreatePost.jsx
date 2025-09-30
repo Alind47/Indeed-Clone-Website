ec7f4bc1dfac3d3c2659feb251db53ade6a180a4
Alind47 <alindkumawat107github@gmail.com>
2025-09-30 16:18:39 +0530

    Update CreatePost.jsx
    Just Added few more frameworks and languages for both developers and data scientist's job roles.

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
-import Header from "../components/Header";
-import { Typography, styled, Box, TextField, Button } from '@mui/material';
-import Dropdown from "../components/Dropdown";
+import Header from "../Components/Header";
+import { Box, Button, TextField, Typography, styled} from '@mui/material';
+import Dropdown from "../Components/Dropdown";
 import { savePost } from "../services/api";
 import { useNavigate } from "react-router-dom";
 import { routePath } from "../routes/route";
 
 const Component = styled(Box)({
-    background: '#F5F5F5',
-    padding: '80px 200px'
+    padding: '80px 200px',
+    background: '#F5F5F5' 
 })
 
 const Container = styled(Box)({
@@ -18,58 +18,61 @@ const Container = styled(Box)({
     justifyContent: 'space-between',
     alignItems: 'center',
     padding: '0 70px',
-    '& > p': {
+    '& > p':{
         fontSize: 35,
-        fontWeight: 700
+        fontWeight: 700,
+        opacity: '0.7'
     }
 })
 
 const FormWrapper = styled(Box)({
+
     display: 'flex',
     flexDirection: 'column',
     marginTop: 20,
     padding: 20,
     background: '#FFFFFF',
     borderRadius: 20,
-    '& > *': {
+    '& > *':{
         marginTop: '20px !important'
     }
 })
 
 const defaultObj = {
-    profile: '',
-    type: '',
-    description: '',
-    experience: '',
-    technology: [],
-    salary: ''
+    profile:"",
+    type:"",
+    description:"",
+    experience:"",
+    technology:[],
+    salary:""
 }
 
 const options = {
-    type: ['Online', 'Offline'],
-    experience: ['0-2 Years', '3-5 Years', '5 Years or more'],
-    technology: ['Java', 'JavaScript', 'React', 'Angular', 'Node.js', 'Docker', 'AWS', 'HTML', 'CSS'],
-    salary: ['Rs 0-300000', 'Rs 300000-500000', 'Rs 500000-800000', 'Rs 800000-1000000', 'Rs 1000000-1500000', 'Rs 1500000-2000000', 'Rs 2000000 or more']
+    type: ["Online","Offline"],
+    experience: ["0-2 Years","2-5 Years","5-8 Years","8 and more Years"],
+    technology: ['Java', 'Python', 'PyTorch', 'TensorFlow', 'Scikit-Learn', 'Pandas', 'JavaScript', 'Spring Boot', 'MongoDB', 'MySQL', 'React.js', 'Express.js', 'Angular.js', 'Node.js', 'Docker', 'AWS', 'Azure', 'Google Cloud', 'Terraform', 'HTML', 'CSS', 'Kotlin', 'Android Studio', 'Firebase', 'Figma', 'Adobe XD', 'Photoshop', 'Illustrator'],
+    salary: ['Rs 0-300000', 'Rs200000-400000', 'Rs 300000-500000', 'Rs 400000-600000', 'Rs 500000-700000', 'Rs 600000-800000', 'Rs 800000-1000000', 'Rs 1000000-1500000', 'Rs 1500000-2000000', 'Rs 2000000 or more']
 }
 
 const CreatePost = () => {
+     
     const [data, setData] = useState(defaultObj);
+    
+    const navigate = useNavigate();
 
     const image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH3zkKYlIHjjoQrE4e-a5xiJIaK0reWlcDhewsx8rjV87d8M82";
-
-    const navigate = useNavigate();
     
     const handleChange = (e) => {
-        setData({ ...data, [e.target.name]: e.target.value });
+         setData({...data, [e.target.name]: e.target.value}); 
     }
 
-    const saveJob = async () => {
+    const saveJob = async() => {
         await savePost(data);
-        navigate(routePath.posts);
+        navigate(routePath.posts)
     }
 
-    return (
-        <>
+    return(
+            <>
             <Header />
             <Component>
                 <Container>
@@ -78,52 +81,52 @@ const CreatePost = () => {
                 </Container>
                 <FormWrapper>
                     <TextField
-                        placeholder="Job title"
-                        onChange={handleChange}
+                        placeholder="Job Title"
                         name="profile"
+                        onChange={handleChange}
                     />
                     <Dropdown 
-                        id='job-type-label'
-                        value={data.type}
-                        handleChange={handleChange}
-                        name="type"
-                        label="Job Type"
-                        options={options.type}
+                      label="Job Type"
+                      id="job-type-label"
+                      value={data.type}
+                      handleChange={handleChange}
+                      name="type"
+                      options={options.type}
                     />
                     <TextField
-                        placeholder="Job description"
-                        onChange={handleChange}
+                        placeholder="Job Description"
                         name="description"
+                        onChange={handleChange}
+                    />    
+                    <Dropdown
+                      label="Experience"
+                      id="job-experience-label"
+                      value={data.experience}
+                      handleChange={handleChange}
+                      options={options.experience} 
+                      name="experience"
                     />
                     <Dropdown 
-                        id='experience-label'
-                        value={data.experience}
-                        handleChange={handleChange}
-                        name="experience"
-                        label="Experience"
-                        options={options.experience}
+                      label="Technology"
+                      id="job-technology-label"
+                      value={data.technology}
+                      handleChange={handleChange}
+                      options={options.technology}
+                      name="technology"
+                      multiple
                     />
                     <Dropdown 
-                        id='technology-label'
-                        value={data.technology}
-                        handleChange={handleChange}
-                        name="technology"
-                        label="Technology"
-                        options={options.technology}
-                        multiple
-                    />
-                    <Dropdown 
-                        id='salary-label'
-                        value={data.salary}
-                        handleChange={handleChange}
-                        name="salary"
-                        label="Salary"
-                        options={options.salary}
+                      label="Salary"
+                      id="job-salary-label"
+                      value={data.salary}
+                      handleChange={handleChange}
+                      options={options.salary}
+                      name="salary"
                     />
                     <Button variant="contained" onClick={() => saveJob()}>Save Job</Button>
                 </FormWrapper>
             </Component>
-        </>
+            </>
     )
 }
 
